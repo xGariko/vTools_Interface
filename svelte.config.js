71deac69a2fdc7196fdaf793a3d8e03ac81875ce
Gabriele Iovino <gabriele.iovino@sistinf.it>
2025-03-24 23:51:43 +0100

    Deploy fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
-import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
