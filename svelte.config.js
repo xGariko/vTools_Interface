c36d80d642fd45e3289fecf9774b3682bd06ce6c
Gabriele Iovino <gabriele.iovino@sistinf.it>
2025-03-25 00:12:01 +0100

    fd

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,13 @@
-import adapter from '@sveltejs/adapter-netlify';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+
 	kit: {
-		adapter: adapter(),
-		// eventuali altre opzioni
-	},
-	preprocess: vitePreprocess()
+		adapter: adapter()
+	}
 };
+
+export default config;
