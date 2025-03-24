8ee67982c01dd3f370a1ce2fb5d1c5ccf908eb92
Gabriele Iovino <gabriele.iovino@sistinf.it>
2025-03-24 23:57:53 +0100

    fd

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,8 @@
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-vercel';
-
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
