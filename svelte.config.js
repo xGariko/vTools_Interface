4a0386028662ba938ae5f86e7a10045aec60316d
Gabriele Iovino <gabrieleiovino839@gmail.com>
2025-11-25 21:59:33 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
